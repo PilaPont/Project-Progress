4ab798ad2c4f87297b02f756be6d7b91452fddef
Maryam Kia <maryam.kia1984@mail.com>
2021-03-31 15:21:25 +0430

    [IMP] project_progress, add _compute_task_progress function and  normal_task_weight field to project.task

--- models/project.py
+++ models/project.py
@@ -13,15 +13,14 @@ class Project(models.Model):
     ])
     task_weighting = fields.Boolean()
     automatic_task_weighting = fields.Boolean(help='based on deliverable items weighting')
-    project_progress = fields.Integer(compute='_compute_project_progress', store=True)
+    project_progress = fields.Float(compute='_compute_project_progress', store=True)
 
     @api.depends('task_ids', 'task_ids.is_closed')
     def _compute_project_progress(self):
         tasks = self.env['project.task'].search([('project_id', 'in', self.ids)])
-
         for project in self:
-            if not project.use_deliverables:
-                project_tasks = tasks.filtered(lambda t: t.project_id == project)
+            project_tasks = tasks.filtered(lambda t: t.project_id == project)
+            if not project.task_weighting:
                 total_tasks_count = len(project_tasks)
                 done_tasks_count = len(project_tasks.filtered(lambda t: t.is_closed))
                 project.project_progress = done_tasks_count / total_tasks_count * 100
@@ -42,9 +41,16 @@ class ProjectTask(models.Model):
             rec.deliverable_items_count = len(total_items)
             rec.done_deliverable_items_count = len(done_items)
 
-    @api.depends()
+    @api.depends('child_ids', 'deliverable_item_ids', 'deliverables_progress')
     def _compute_task_progress(self):
-        pass
+        for task in self:
+            if task.is_closed:
+                task.task_progress = 100
+            elif task.child_ids:
+                task.task_progress = sum(
+                    subtask.task_progress * subtask.normal_task_weight for subtask in task.child_ids) * 100
+            elif task.deliverable_item_ids:
+                task.task_progress = task.deliverables_progress
 
     @api.depends('task_weighting')
     def _compute_task_weight(self):
@@ -52,8 +58,22 @@ class ProjectTask(models.Model):
             if not task.task_weighting:
                 task.task_weight = 1
 
-    task_weight = fields.Integer(compute='_compute_task_weight', store=True)
-    task_progress = fields.Integer(compute='_compute_task_progress', store=True)
+    @api.depends('task_weight')
+    def _compute_normal_task_weight(self):
+        projects_sum_task_weight = self.read_group([('project_id', 'in', self.mapped('project_id.id'))],
+                                                   ['project_id', 'task_weight:sum(task_weight)'],
+                                                   groupby=['project_id'])
+        projects_sum_task_weight_dict = dict()
+        for item in projects_sum_task_weight:
+            projects_sum_task_weight_dict[item['project_id'][0]] = item['task_weight']
+        for task in self:
+            import logging
+            logging.critical(projects_sum_task_weight_dict)
+            task.normal_task_weight = task.task_weight / projects_sum_task_weight_dict[task.project_id.id]
+
+    task_weight = fields.Float(compute='_compute_task_weight', store=True)
+    normal_task_weight = fields.Float(compute='_compute_normal_task_weight')
+    task_progress = fields.Float(compute='_compute_task_progress', store=True, default=0)
     deliverable_item_ids = fields.One2many('deliverable.item', 'task_id', string='Check List')
     deliverables_progress = fields.Float(compute='compute_deliverables_stats', string='Weighted progress', store=True)
     deliverable_items_count = fields.Integer(compute='compute_deliverables_stats', string='Total Deliverable Items',
