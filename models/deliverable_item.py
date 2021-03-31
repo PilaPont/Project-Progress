afbd301c9d439e1952ffb0da440b2cd69b001303
Maryam Kia <maryam.kia1984@mail.com>
2021-03-31 11:38:42 +0430

    [IMP] project_progress, add task_weight, task_progress and task_weighting fields to project.task

--- models/deliverable_item.py
+++ models/deliverable_item.py
@@ -1,30 +1,6 @@
 from odoo import api, models, fields, exceptions, _
 
 
-class ProjectTask(models.Model):
-    _inherit = "project.task"
-
-    @api.depends('deliverable_item_ids', 'deliverable_item_ids.done', 'deliverable_item_ids.weight')
-    def compute_deliverables_stats(self):
-        """:return the value for the deliverable items progress"""
-        for rec in self:
-            total_items = rec.deliverable_item_ids
-            done_items = rec.deliverable_item_ids.filtered('done')
-            total_weight = sum(total_items.mapped('weight'))
-            if total_weight:  # constrains are checked at create time so it is possible to have zero weight at run time
-                rec.deliverables_progress = sum(done_items.mapped('weight')) / total_weight * 100
-            rec.deliverable_items_count = len(total_items)
-            rec.done_deliverable_items_count = len(done_items)
-
-    deliverable_item_ids = fields.One2many('deliverable.item', 'task_id', string='Check List')
-    deliverables_progress = fields.Float(compute='compute_deliverables_stats', string='Weighted progress', store=True)
-    deliverable_items_count = fields.Integer(compute='compute_deliverables_stats', string='Total Deliverable Items',
-                                             store=True)
-    done_deliverable_items_count = fields.Integer(compute='compute_deliverables_stats', string='Done Deliverable Items',
-                                                  store=True)
-    use_deliverables = fields.Boolean(related='project_id.use_deliverables')
-
-
 class DeliverableItem(models.Model):
     _name = 'deliverable.item'
     _description = 'Deliverable Items'
--- models/project.py
+++ models/project.py
@@ -18,9 +18,47 @@ class Project(models.Model):
     @api.depends('task_ids', 'task_ids.is_closed')
     def _compute_project_progress(self):
         tasks = self.env['project.task'].search([('project_id', 'in', self.ids)])
+
         for project in self:
             if not project.use_deliverables:
                 project_tasks = tasks.filtered(lambda t: t.project_id == project)
                 total_tasks_count = len(project_tasks)
                 done_tasks_count = len(project_tasks.filtered(lambda t: t.is_closed))
                 project.project_progress = done_tasks_count / total_tasks_count * 100
+
+
+class ProjectTask(models.Model):
+    _inherit = "project.task"
+
+    @api.depends('deliverable_item_ids', 'deliverable_item_ids.done', 'deliverable_item_ids.weight')
+    def compute_deliverables_stats(self):
+        """:return the value for the deliverable items progress"""
+        for rec in self:
+            total_items = rec.deliverable_item_ids
+            done_items = rec.deliverable_item_ids.filtered('done')
+            total_weight = sum(total_items.mapped('weight'))
+            if total_weight:  # constrains are checked at create time so it is possible to have zero weight at run time
+                rec.deliverables_progress = sum(done_items.mapped('weight')) / total_weight * 100
+            rec.deliverable_items_count = len(total_items)
+            rec.done_deliverable_items_count = len(done_items)
+
+    @api.depends()
+    def _compute_task_progress(self):
+        pass
+
+    @api.depends('task_weighting')
+    def _compute_task_weight(self):
+        for task in self:
+            if not task.task_weighting:
+                task.task_weight = 1
+
+    task_weight = fields.Integer(compute='_compute_task_weight', store=True)
+    task_progress = fields.Integer(compute='_compute_task_progress', store=True)
+    deliverable_item_ids = fields.One2many('deliverable.item', 'task_id', string='Check List')
+    deliverables_progress = fields.Float(compute='compute_deliverables_stats', string='Weighted progress', store=True)
+    deliverable_items_count = fields.Integer(compute='compute_deliverables_stats', string='Total Deliverable Items',
+                                             store=True)
+    done_deliverable_items_count = fields.Integer(compute='compute_deliverables_stats', string='Done Deliverable Items',
+                                                  store=True)
+    use_deliverables = fields.Boolean(related='project_id.use_deliverables')
+    task_weighting = fields.Boolean(related='project_id.task_weighting')
